2cc7dc036ae6435bafa2af93da6e7a2e5438ed8f
Emma Egstad <emma@egstad.com>
2022-02-14 16:56:18 -0800

    setting up useReducer

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import { useReducer } from 'react';
 import './Home.css';
 import List from '../../components/List/List';
 import Form from '../../components/Form/Form';
 
+const initialItems = [
+  { id: 0, text: 'Oat Milk', done: false },
+  { id: 1, text: 'Parmesan Crisps', done: false },
+  { id: 2, text: 'Lacinato Kale', done: false },
+];
+
+function itemsReducer(items, action) {
+  switch (action.type) {
+    case 'added': {
+      return [
+        ...items,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case 'edited': {
+      return items.map((item) => {
+        if (item.id === action.task.id) {
+          return action.task;
+        }
+        return item;
+      });
+    }
+    case 'deleted': {
+      return items.filter((item) => item.id !== action.id);
+    }
+    default: {
+      throw Error(`Unknown action: ${action.type}`);
+    }
+  }
+}
+
 export default function Home() {
+  const [items, dispatch] = useReducer(itemsReducer, initialItems);
+
   return (
     <div className="Home">
       <List />
